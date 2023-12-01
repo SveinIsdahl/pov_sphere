93357163c100f71f712f0073209c363b0134cb71
SveinIsdahl <45757028+SveinIsdahl@users.noreply.github.com>
2023-12-01 13:00:13 +0100

    Update usart.c for atmega88a

--- usart.c
+++ usart.c
@@ -5,18 +5,17 @@ void usart_init(uin16_t ubrr) {
   UBRRH = (uint8_t)(ubrr >> 8);
   UBRRL = (uint8_t)ubrr;
   
-  //Enable receiver and transmitter
-  UCSRB = (1<<RXEN)|(1<<TXEN);
-  
-  //Set frame format: 8 data bits, 1 stop bit
-  UCSRC = (1<<URSEL)|(3<<UCSZ0);
+  Enable receiver and transmitter */
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  /* Set frame format: 8data, 1stop bit */
+  UCSR0C = (3<<UCSZ00);
 }
 
 void usart_transmit(char data) {
   //Wait tfor empty transmit buffer
   while (!(UCSRA & (1<<UDRE)));
   
-  UDR = data;
+  UDR0 = data;
 }
 
 void usart_transmit_string(char *data) {
